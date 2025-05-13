996f375758afbd7cc513f5b2567eb9d3820ae00e
Menoti Filho <menotimfilho@gmail.com>
2025-05-13 16:59:23 -0300

    host change

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,17 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000"], // Permita o frontend local
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
